45aa70791d467fc39fd0a4e568fe497523a7fb93
Klement Omeri <klementomeri97@gmail.com>
2022-10-21 17:02:40 +0200

    initial commit

---
+++ deck.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	suits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "J", "Q", "K"}
+
+	for _, suit := range suits {
+		for _, value := range values {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	byteSlice, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	cards := strings.Split(string(byteSlice), ",")
+	deck := deck(cards)
+
+	return deck
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
---
+++ deck_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeckLength(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Error("Expected deck length of 52 but got ", len(d))
+	}
+}
+
+func TestNewDeckFirstCard(t *testing.T) {
+	d := newDeck()
+
+	if d[0] != "Ace of Spades" {
+		t.Error("Expected Ace of Spaces but got ", d[0])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	filename := "_decktesting"
+	os.Remove(filename)
+
+	deck := newDeck()
+	deck.saveToFile(filename)
+
+	loadedDeck := newDeckFromFile(filename)
+
+	if len(deck) != len(loadedDeck) {
+		t.Error("Expected to have the same length from both decks.")
+	}
+}
